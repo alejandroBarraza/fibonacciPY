10a4ba4f25b7d92af8f579d9af6cd31e14d74e15
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-01-21 23:23:40 -0300

    fibonacci parallel factorization working

--- fibonacci.py
+++ fibonacci.py
@@ -1,48 +1,54 @@
+import multiprocessing
+import multiprocessing as mp
 
+#function for calculate fibannaci number
 def fib(n):
-    
+
     if n<2:
         return n
     else:
         return (fib(n-1) + fib(n-2))
-        
-        
+
+#calcute the factor of fibonacci sequence
+def fac(result):
+    
+    divide=2
+    if result<6:
+        print(result)
+    else:
+        while result !=1:
+            if result % divide == 0:
+                print(divide,end=" ")
+                result = result/divide
+                if result !=1:
+                    print("x ",end=" ")
+                        
+            else:
+                divide+=1
+            
+    print("")
     
 
+#main class        
 def main():
     
     print('Fibonacci Sequence')
+    # paralleing throught pool.
+    p = multiprocessing.Pool(mp.cpu_count())
+    n=0
 
-    # to the first 300 numbers
-    for i in range(60):
-        index = i+1
-        
-        result = fib(i)
-        print(i,":",result,"=",end=" ")
-        #//calcute the factor of fibonacci sequence
-        divide=2
-        if result<6:
-            print(result)
-        else:
-            while result !=1:
-                if result % divide == 0:
-                    print(divide,end=" ")
-                    result = result/divide
-                    if result !=1:
-                        print("x ",end=" ")
-                        
-                else:
-                    divide+=1
-            
-        print("")
-        
-
+    #the first 300 fibonacci numbers
+    while(n<300):
+        number = p.map(fib,[n]) 
+        #result = fib(i)
+        print("fibo",n,":", number , "=",end=" ")
+        fac(number[0])
+        n+=1      
     print('Finish')
     
-
-
-
+#calling main class    
 if __name__ == "__main__":
+    
     main()
 
                 
